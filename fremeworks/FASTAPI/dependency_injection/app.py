06c6ad1fe7a3037cf7f9e2ce1c3204a7ed860dce
_Ankitdeveloper <ankit.prajapati@scriptalldna.com>
2026-02-15 18:59:21 +0530

    with assure dependency

--- fremeworks/FASTAPI/dependency_injection/app.py
+++ fremeworks/FASTAPI/dependency_injection/app.py
@@ -1,38 +1,53 @@
-from fastapi import FastAPI, APIRouter, HTTPException, Depends
-import uvicorn
-from pydantic import BaseModel, model_validator 
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, Header
+from pydantic import BaseModel, EmailStr, Field, model_validator, field_validator
 import datetime
 
-valid_user = ["ashu"] 
+app = FastAPI(title="Denpendency Injection!!")
+routers = APIRouter(prefix="/with_denpendency", tags=["Dependency Injection"])
 
 class PreCheckToDo(BaseModel):
     work: str
     duration: int
-    start_time:datetime.datetime = datetime.datetime.now() 
+    start_time: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
     @model_validator(mode="after")
-    def validate_fields(self) -> 'PreCheckToDo':
-        if "ai" in self.work.lower(): return self
-        raise HTTPException(detail="AI task is acceptable !!", status_code=422)
-        
-app = FastAPI(title="Denpendency Injection!!")
-routers = APIRouter(prefix="/with_denpendency", tags=["Dependency Injection"])
+    def validate_fields(self):
+        if "ai" in self.work.lower():
+            return self
+        raise ValueError("AI task is acceptable only")
 
-@app.get("/health_check")
-async def health_check():
-    return "Good Heath!!"
+class PreCheckUsers(BaseModel):
+    name: str
+    email: EmailStr
 
-async def dependenct_system_users():
-    if "ashu" in valid_user:
-        return valid_user
-    raise HTTPException(detail="Not Valid User", status_code=401)
+    @field_validator("email", mode="after")
+    @classmethod
+    def drl_email_validator(cls, v):
+        if v.endswith("@drreddys.com"):
+            return v
+        raise ValueError("Allow Drreddys Emails Only")
+
+def dependency_system_users(todo: PreCheckToDo) -> int:
+    return todo.duration
+
+async def get_token(Authorization: str = Header(...)):
+    if not Authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401)
+    return Authorization
+
+async def dependency_users(
+    user: PreCheckUsers,
+    token: str = Depends(get_token)
+):
+    return user
 
 @routers.post("/todo")
-async def my_todo(todo: PreCheckToDo, token:list = Depends(dependenct_system_users)):
-    print(todo)
-    print(token)
-    return f"Added Todo {todo.work}"
-    
+async def my_todo(
+    duration = Depends(dependency_system_users),
+    user = Depends(dependency_users)
+):
+    return {"duration": duration, "user": user}
+
 app.include_router(routers)
 
 if __name__ == "__main__":
