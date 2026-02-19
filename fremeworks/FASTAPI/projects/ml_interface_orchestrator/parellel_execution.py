ece3083e2e5e53c0af2c185bc100b404c36230a5
_Ankitdeveloper <ankit.prajapati@scriptalldna.com>
2026-02-20 01:02:29 +0530

    add new method to execute parallel.

--- fremeworks/FASTAPI/projects/ml_interface_orchestrator/parellel_execution.py
+++ fremeworks/FASTAPI/projects/ml_interface_orchestrator/parellel_execution.py
@@ -1,2 +1,11 @@
+from concurrent.futures import InterpreterPoolExecutor
+import asyncio
+from functools import partial
 
-
+async def execut_parellel_with_interpreter_pool(__fun__, *args, **kwargs):
+    """
+    Execut Fun in a separate interpreter with leverage the new feature of python 3.14.
+    """
+    running_loop = asyncio.get_running_loop()
+    with InterpreterPoolExecutor(max_workers=1) as interpreter_executor:
+        running_loop.run_in_executor(interpreter_executor, partial(__fun__, *args, **kwargs))
